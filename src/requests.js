721c989ffdcc86c4b4d45b78e7a04004e791b918
yupseth <cristinayup@gmail.com>
2023-03-15 14:26:46 +0200

    make requests file

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "7f9da03c80170cd7df2dbdfa5c492e89";
+
+export default {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchAnimation: `discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchVideo: `/movie/{movie_id}/videos?api_key=${API_KEY}`,
+};
