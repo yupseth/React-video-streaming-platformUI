f5c5d16d5687aa0a86ef879a8742f1ea953a0613
yupseth <cristinayup@gmail.com>
2023-03-24 16:56:19 +0200

    delete comment

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -60,7 +60,6 @@ const Nav = ({ results, setResults }) => {
             results={results}
           />{" "}
         </Link>
-        {/* {results.length > 0 && <SearchResultPage results={results} />} */}
 
         <img
           onClick={onProfileClick}
