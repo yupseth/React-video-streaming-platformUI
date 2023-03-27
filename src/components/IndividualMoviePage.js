dacbc6cb97b8343d51c9824a01ea8edc4fb2c7d9
yupseth <cristinayup@gmail.com>
2023-03-27 14:25:40 +0300

    minor changes

--- src/components/IndividualMoviePage.js
+++ src/components/IndividualMoviePage.js
@@ -3,16 +3,18 @@ import style from "./IndividualMoviePage.module.scss";
 const IndividualMoviePage = () => {
   return (
     <div className={style.individual_page}>
-      <header className={style.banner}>
-        <h1>this is the banner</h1>
-      </header>
+      <div className={style.individual_page__content}>
+        <header className={style.banner}>
+          <h1>this is the banner</h1>
+        </header>
 
-      <div className={style.movieInfo}>
-        <h3>this is some content about the movie</h3>
-      </div>
+        <div className={style.movieInfo}>
+          <h3>this is some content about the movie</h3>
+        </div>
 
-      <div className={style.trailer_area}>
-        <h3>here is the trailer</h3>
+        <div className={style.trailer_area}>
+          <h3>here is the trailer</h3>
+        </div>
       </div>
     </div>
   );
